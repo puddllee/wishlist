ca4a8911e4f9836fb6552fd54b6065832e5799ff
tbattistella <tyson@telmediq.com>
2015-12-12 01:24:33 -0800

    OAuth config

---
+++ server/config.js
@@ -0,0 +1,20 @@
+ServiceConfiguration.configurations.remove({
+	service: "google"
+});
+
+ServiceConfiguration.configurations.insert({
+	service: "google",
+	clientId: "738048454565-1en1rhvonv9832ok9qdv6gdd5bbdgdfm.apps.googleusercontent.com",
+	secret: "COqDfAjsZRcwpGEb_83I3zi5",
+	loginStyle: "popup"
+});
+
+ServiceConfigurations.configurations.remove({
+	service: "facebook"
+});
+
+ServiceConfigurations.configurations.insert({
+	service: "facebook",
+	appId: "130018900700595",
+	secret: "087d7b3aa5534cf672e78a524869ea9a"
+});
