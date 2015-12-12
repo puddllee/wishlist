2b035940b9ad65724878e70788c381ecef9ec4d1
Jake Runzer <jakerunzer@gmail.com>
2015-12-12 04:04:06 -0800

    list ui

---
+++ client/views/common/addWish.js
@@ -0,0 +1,59 @@
+Template.addWish.rendered = function() {
+  Session.set('amazon-active', false);
+  Session.set('addWishError', '');
+}
+
+Template.addWish.events({
+  'click #amazon-tab': function() {
+    Session.set('amazon-active', true);
+    Session.set('addWishError', '');
+  },
+
+  'click #text-tab': function() {
+    Session.set('amazon-active', false);
+    Session.set('addWishError', '');
+  },
+
+  'submit .add-wish': function(event) {
+    event.preventDefault();
+
+    if (Session.get('amazon-active')) {
+      // amazon wish
+      var url = event.target.amazonurl.value;
+      if (validateInput(url)) {
+        Session.set('addWishError', '');
+        console.log('AMAZON: ' + url);
+      } else {
+        Session.set('addWishError', 'Amazon url required');
+      }
+    } else {
+      // text wish
+      var name = event.target.name.value;
+      var seller = event.target.seller.value;
+      var price = event.target.price.value;
+      var url = event.target.url.value;
+      var detail = event.target.detail.value;
+
+      if (validateInput(name) && validateInput(seller) && validateInput(price)) {
+        Session.set('addWishError', '');
+        console.log('name: ' + name);
+        console.log('seller: ' + seller);
+        console.log('price: ' + price);
+        console.log('url: ' + url);
+        console.log('detai: ' + detail);
+      } else {
+        Session.set('addWishError', 'Missing required fields');
+      }
+    }
+  }
+});
+
+Template.addWish.helpers({
+  amazonActive: function() {
+    return Session.get('amazon-active') ? "active-tab" : "";
+  },
+
+  textActive: function() {
+    return Session.get('amazon-active') ? "" : "active-tab";
+  }
+});
---
+++ client/views/list.js
@@ -0,0 +1,85 @@
+var yourList = [{
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Another Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}, {
+  title: 'Product Title',
+  url: 'http://puddllee.io',
+  seller: 'Walmart',
+  detail: 'This is some detail about the product',
+  image: 'https://store.storeimages.cdn-apple.com/4876/as-images.apple.com/is/image/AppleInc/aos/published/images/M/KX/MKXJ2/MKXJ2?wid=445&hei=445&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1441818627105',
+  price: '$40',
+  bought: false,
+  bought_user: 'Jake',
+  type: 'text'
+}];
+
+var friendList = [];
+
+var SAVE_WAIT_TIME = 1000; // ms
+var last_save;
+
+Template.list.rendered = function() {
+  Session.set('yourList', yourList);
+  Session.set('friendList', friendList);
+}
+
+Template.list.events({});
+
+Template.list.helpers({});
