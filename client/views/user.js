16da234eee6d9d739e9afc2b6ed4e457088c0c48
tbattistella <tyson@telmediq.com>
2015-12-13 04:22:15 -0800

    semicolons

--- client/views/user.js
+++ client/views/user.js
@@ -3,16 +3,16 @@ Template.user.helpers = ({
     var controller = User.controller();
     return controller.state.get('userId');
   }
-})
+});
 
 Template.user.events = ({
 
-})
+});
 
 Template.user.rendered = function() {
   console.log(this);
-  Session.set('user', this.data)
+  Session.set('user', this.data);
   Session.set('wishList', Wishlists.findOne({
     'owner': this.data._id
-  }))
-}
+  }));
+};
