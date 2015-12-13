290238662e297f96071e8a31efb536c5ac19a546
tbattistella <tyson@telmediq.com>
2015-12-13 04:21:03 -0800

    render users wishlist

--- client/views/user.js
+++ client/views/user.js
@@ -10,7 +10,9 @@ Template.user.events = ({
 })
 
 Template.user.rendered = function() {
-
   console.log(this);
   Session.set('user', this.data)
+  Session.set('wishList', Wishlists.findOne({
+    'owner': this.data._id
+  }))
 }
