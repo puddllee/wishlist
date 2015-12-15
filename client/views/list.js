863cfa80f5c377f8597c7ccf2b2501556a33701a
Jake Runzer <jakerunzer@gmail.com>
2015-12-14 23:09:10 -0800

    fix when friend request noti sent and formatting

--- client/views/list.js
+++ client/views/list.js
@@ -1,7 +1,7 @@
 var SAVE_WAIT_TIME = 1000; // ms
 var last_save;
 
-Template.list.rendered = function () {
+Template.list.rendered = function() {
   Session.set('yourList', []);
   // var wishlist = Wishlists.findOne({
   //   owner: Meteor.userId()
@@ -9,7 +9,7 @@ Template.list.rendered = function () {
   // Session.set('wishlist', wishlist);
   // console.log(wishlist);
 
-  Meteor.call('getWishlist', Meteor.userId(), function (error, wishlist) {
+  Meteor.call('getWishlist', Meteor.userId(), function(error, wishlist) {
     if (wishlist) {
       Session.set('wishlist', wishlist);
       Session.set('isMine', Meteor.userId() === wishlist.owner);
@@ -18,9 +18,10 @@ Template.list.rendered = function () {
 }
 
 Template.list.helpers({
-  wishlist: function () {
+  wishlist: function() {
     var user = Meteor.userId(); // should make this get recalled when user changes
-    Meteor.call('getWishlist', Meteor.userId(), function (error, wishlist) {
+    console.log('getting wishlist');
+    Meteor.call('getWishlist', Meteor.userId(), function(error, wishlist) {
       if (wishlist) {
         console.log('setting here');
         Session.set('wishlist', wishlist);
