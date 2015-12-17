22d5971513995ae543a4f7b2e39cc28e9d40a3d1
Jake Runzer <jakerunzer@gmail.com>
2015-12-16 23:18:32 -0800

    remove migrate http avatar code
    already ran on server

--- server/migrate.js
+++ server/migrate.js
@@ -1,19 +1,1 @@
 // Run code once and remove to migrate db or whatnot when needed
-
-var users = Meteor.users.find({});
-
-// removes non http from users at startup
-users.forEach(function(u) {
-  if (u.profile.avatar.indexOf('http://www.gravatar.com/') !== -1) {
-    var av = u.profile.avatar.replace('http://www.gravatar.com/', 'https://secure.gravatar.com/');
-    console.log(av);
-    u.profile.avatar = av;
-    Meteor.users.update({
-      _id: u._id
-    }, {
-      $set: {
-        "profile.avatar": av
-      }
-    });
-  }
-});
