40e3efa5a53dc4d22511db8e115d60acc6a6475a
Jake Runzer <jakerunzer@gmail.com>
2015-12-17 00:18:07 -0500

    Better build

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-rm -rf build/
-meteor build build
-cd build
+#rm -rf build/
+
+echo 'Cleaning build_tmp dir'
+rm -r build_tmp
+
+echo 'Building meteor to build_tmp'
+meteor build build_tmp
+
+echo 'Untaring'
+cd build_tmp
 tar -zxf wishlist.tar.gz
+
+echo 'Npm Install'
 cd bundle/programs/server
 npm install
 cd ../../../..
+
+echo 'Removing build'
+rm -r build
+
+echo 'Moving build_tmp to build'
+mv build_tmp build
+
