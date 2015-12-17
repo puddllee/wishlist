b35f4667881f8953edfe102375a4ecbd52abcfa6
Jake Runzer <jakerunzer@gmail.com>
2015-12-17 03:48:59 -0800

    add get items for list method to server

--- server/list.js
+++ server/list.js
@@ -106,8 +106,32 @@ Wishlist = {
     Items.remove(itemId);
   },
 
-  getItemsForList: function(userId) {
-
+  getItemsForList: function(wishlist) {
+    var items = [];
+    if (wishlist) {
+      if (wishlist.owner === Meteor.userId()) {
+        items = Items.find({
+          wishlist: wishlist._id
+        }, {
+          sort: {
+            created_at: -1
+          },
+          fields: {
+            bought: 0,
+            bought_id: 0
+          }
+        }).fetch();
+      } else {
+        items = Items.find({
+          wishlist: wishlist._id
+        }, {
+          sort: {
+            created_at: -1
+          }
+        }).fetch();
+      }
+    }
+    return items;
   },
 
   getWishlist: function(userId) {
