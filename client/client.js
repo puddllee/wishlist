b709967c2d584b79a3c1906905fa13965b0b628e
Jake Runzer <jakerunzer@gmail.com>
2015-12-22 00:40:55 -0700

    fix annoying streamy console messages

--- client/client.js
+++ client/client.js
@@ -3,3 +3,14 @@ Accounts.onResetPasswordLink(function(token, done) {
     token: token
   })
 });
+
+// remove annoying debug messages from streamy
+Meteor._debug = (function(super_meteor_debug) {
+  return function(error, info) {
+    if (info && _.has(info, 'msg'))
+    // super_meteor_debug("Streamy message is allowed!", info)
+    ;
+    else
+      super_meteor_debug(error, info);
+  }
+})(Meteor._debug);
